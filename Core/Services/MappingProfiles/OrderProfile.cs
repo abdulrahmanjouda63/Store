02b16d901474d213501239c1d0ae8ad0186fc839
abdulrahmanjouda63 <100361517+abdulrahmanjouda63@users.noreply.github.com>
2025-05-03 12:34:05 +0300

    New Fix About Session 05

--- Core/Services/MappingProfiles/OrderProfile.cs
+++ Core/Services/MappingProfiles/OrderProfile.cs
@@ -14,15 +14,19 @@ namespace Services.MappingProfiles
         public OrderProfile()
         {
             CreateMap<Address, AddressDto>().ReverseMap();
+
             CreateMap<OrderItem, OrderItemDto>()
                 .ForMember(D => D.ProductId, O => O.MapFrom(S => S.ProductInOrderItem.ProductId))
                 .ForMember(D => D.ProductName, O => O.MapFrom(S => S.ProductInOrderItem.ProductName))
                 .ForMember(D => D.PictureUrl, O => O.MapFrom(S => S.ProductInOrderItem.PictureUrl))
                 ;
+
             CreateMap<Order, OrderResultDto>()
                 .ForMember(D => D.PaymentStatus, O => O.MapFrom(S => S.PaymentStatus.ToString()))
                 .ForMember(D => D.DeliveryMethod, O => O.MapFrom(S => S.DeliveryMethod.ShortName))
                 .ForMember(D => D.Total, O => O.MapFrom(S => S.SubTotal + S.DeliveryMethod.Cost));
+
+            CreateMap<DeliveryMethod, DeliveryMethodDto>();
         }
     }
 }
--- Shared/OrderModels/DeliveryMethodDto.cs
+++ Shared/OrderModels/DeliveryMethodDto.cs
@@ -10,8 +10,8 @@ namespace Shared.OrderModels
     {
         public int Id { get; set; }
         public string ShortName { get; set; }
-        public string DeliveryTime { get; set; }
         public string Description { get; set; }
+        public string DeliveryTime { get; set; }
         public decimal Cost { get; set; }
     }
 }
